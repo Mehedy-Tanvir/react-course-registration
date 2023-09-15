a4af491c06637008c4a068fd3d51b8fce9dc2165
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-09-15 14:20:38 +0600

    add header section

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="font-inter text-[#1C1B1B] text-[32px] font-bold text-center">
+        Course Registration
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
